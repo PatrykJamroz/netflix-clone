d716628456a89999fc9afc7ebd0cece50f244220
Patryk Jamróz <jamroz.patryk@gmail.com>
2021-05-17 21:14:24 +0200

    2nd commit, jumbotron added

---
+++ src/pages/home.js
@@ -0,0 +1,7 @@
+import Jumbotron from "../components/jumbotron";
+import React from "react";
+import { JumbotronContainer } from "../containers/jumbotron";
+
+export default function Home() {
+  return <JumbotronContainer />;
+}
