ef1e3571e871eadeeeb39a4d8c94a0c1edb68c65
Patryk Jamróz <jamroz.patryk@gmail.com>
2021-05-18 22:49:24 +0200

    12-13 footer component added

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,7 +1,13 @@
 import Jumbotron from "../components/jumbotron";
 import React from "react";
 import { JumbotronContainer } from "../containers/jumbotron";
+import { FooterContainer } from "../containers/footer";
 
 export default function Home() {
-  return <JumbotronContainer />;
+  return (
+    <>
+      <JumbotronContainer />
+      <FooterContainer />
+    </>
+  );
 }
